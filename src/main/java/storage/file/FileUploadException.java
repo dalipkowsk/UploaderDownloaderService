017525bd5bf606b8f1b203593773a738a6a59b69
hubert.lapsa <hubert.lapsa@gmail.com>
2018-11-11 22:56:32 +0100

    Fix error handling.

--- src/main/java/storage/file/FileUploadException.java
+++ src/main/java/storage/file/FileUploadException.java
@@ -1,16 +1,11 @@
 package storage.file;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.io.IOException;
 
 public class FileUploadException extends IOException {
 
-    private static final Logger log = LogManager.getLogger(FileUploadException.class);
-
     FileUploadException(IOException exception){
 
-        log.error(exception.getMessage());
+        super("Error while upload file");
     }
 }
